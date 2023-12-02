7e5fabbf88ce86b556342e39941cb454e87511ba
Rafael Biehler <mail@rafaelbiehler.com>
2023-12-02 16:51:14 +0100

    day2 part one

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,38 @@
 import re
 
 
+def part_one(lines: list[str]):
+    possible_game_ids = []
+
+    for line in lines:
+        m = re.match(r"Game (\d+): ", line)
+        game_id = int(m.group(1))
+
+        # Remove part with game information
+        line = line[m.end():]
+        game_is_possible = []
+
+        # Count number of cubes in each part of the game
+        for i, game_part in enumerate(line.split(";")):
+            blue = 0
+            green = 0
+            red = 0
+
+            if m := re.search(r"(\d+) blue", game_part):
+                blue = int(m.group(1))
+            if m := re.search(r"(\d+) green", game_part):
+                green = int(m.group(1))
+            if m := re.search(r"(\d+) red", game_part):
+                red = int(m.group(1))
+
+            game_is_possible.append(red <= 12 and green <= 13 and blue <= 14)
+
+        if all(game_is_possible):
+            possible_game_ids.append(game_id)
+
+    print("Answer:", sum(possible_game_ids))
+
+
 def part_two(lines: list[str]):
     cube_sets = []
 
@@ -42,4 +74,5 @@ if __name__ == '__main__':
     with open("input.txt", mode="r", encoding="utf-8-sig") as f:
         lines = [l.strip() for l in f.readlines()]
 
+    part_one(lines)
     part_two(lines)
